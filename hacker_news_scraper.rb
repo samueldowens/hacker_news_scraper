4bbef949b176240bf2983a4b60711993584c5141
samueldowens <samueldowens@gmail.com>
2013-10-14 15:38:59 -0400

    first commit

---
+++ hacker_news_scraper.rb
@@ -0,0 +1,8 @@
+#should use json to get a decent file from the hacker news page
+#should use nokogiri to scrape the site
+#should take any post with more than X number of upvotes and put into database
+#database should store title, link, upvotes, ?
+
+require 'json'
+require 'nokogiri'
+require 'sqlite3'
